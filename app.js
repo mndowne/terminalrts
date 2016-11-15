fba0ad660ad4d676a832af7ad0433000bf0bf498
mndowne <mndowne@gmail.com>
2016-11-14 21:18:06 -0600

    getting going here

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/client/css/'));
+app.use(express.static(__dirname + '/client/'));
+
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/client/index.html');
+});
+
 
 app.listen(3000, function(){
   console.log('Server is up on port 3000');
